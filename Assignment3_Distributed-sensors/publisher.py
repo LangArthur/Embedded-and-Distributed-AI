45fbb80cbbdb223d589e8a6a5c95dd0661c7af67
alang <arthur.lang@epitech.eu>
2021-05-10 12:53:43 +0200

    add publisher and init subscriber

---
+++ Assignment3_Distributed-sensors/publisher.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Created on Mon May 10 2021
+#
+# Arthur Lang
+# publisher.py
+#
+
+import paho.mqtt.client as mqtt
+import time
+import sys
+import numpy
+import datetime
+
+MQT_SERVER = "test.mosquitto.org"
+
+def readSensorData():
+    mu, sigma = 1200.00, 1.0
+    reading = f'{round(numpy.random.normal(mu, sigma), 2):.2f}'        
+    dt = datetime.datetime.now()
+    dt = dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+    message = f'{reading}|{dt}'
+    return message
+
+def publisher():
+    client = mqtt.Client("Subscriber")
+    channel = "teds20/group07/pressure"
+
+    try:
+        client.connect(MQT_SERVER)
+        client.loop_start()
+        client.subscribe(channel)
+        print("Ready to publish")
+        # loop to send 10 messages
+        for _ in range(10):
+            message = readSensorData()
+            print("Send: {}".format(message))
+            client.publish(channel, message, qos=2)
+            time.sleep(1)
+        client.loop_stop()
+        client.disconnect()
+        print("Disconnected")
+    except Exception as err:
+        print("An error occured: {}".format(err), file=sys.stderr)
+    return 0
+
+if __name__ == "__main__":
+    publisher()
---
+++ Assignment3_Distributed-sensors/subscriber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Created on Mon May 10 2021
+#
+# Arthur Lang
+# subscriber.py
+#
+
+def subscriber():
+    return 0
+
+if __name__ == "__main__":
+    subscriber()
